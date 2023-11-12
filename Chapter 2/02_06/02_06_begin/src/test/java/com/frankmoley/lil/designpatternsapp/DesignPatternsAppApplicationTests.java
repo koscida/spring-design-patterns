9c0a259b050284be71e7a9fcdbe78b0dcc6ee2f6
koscida <britkos@gmail.com>
2023-11-11 21:11:58 -0500

    add 2.6

--- Chapter 2/02_06/02_06_begin/src/test/java/com/frankmoley/lil/designpatternsapp/DesignPatternsAppApplicationTests.java
+++ Chapter 2/02_06/02_06_begin/src/test/java/com/frankmoley/lil/designpatternsapp/DesignPatternsAppApplicationTests.java
@@ -1,7 +1,11 @@
 package com.frankmoley.lil.designpatternsapp;
 
+import com.frankmoley.lil.designpatternsapp.singleton.SingA;
+import com.frankmoley.lil.designpatternsapp.singleton.SingB;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +13,22 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DesignPatternsAppApplicationTests {
 
+    @Autowired
+    SingB singB1;
+
+    @Autowired
+    SingB singB2;
+
     @Test
-    public void contextLoads() {
+    public void testSingletons() {
+        SingA singA1 = SingA.getInstance();
+        SingA singA2 = SingA.getInstance();
+
+        Assert.assertNotNull(singA1);
+        Assert.assertSame(singA1, singA2);
+
+        Assert.assertNotNull(singB1);
+        Assert.assertSame(singB1, singB2);
     }
 
 }
